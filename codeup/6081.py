ccc27c1b43c63679c254599fe82d6b015149eff7
hyojun6 <yhyojun66@gmail.com>
2024-09-18 22:36:53 +0900

    codeup

--- codeup/6081.py
+++ codeup/6081.py
@@ -1,7 +1,7 @@
 n = input()
 a = int(n, 16)
-for i in range(1, 16):
+for i in range(1,16):
     num = a * i
     hex_num = hex(num)[2:].upper()
     hex_i = hex(i)[2:].upper()
-    print(f"{n.upper()}*{hex_i}={hex_num}")
+    print(f"{n.upper()}*{hex_i}={hex_num}" )
