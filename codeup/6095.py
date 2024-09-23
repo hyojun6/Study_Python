1d2b75b0c889a5862351269df0a19faace4234b0
hyojun6 <yhyojun66@gmail.com>
2024-09-23 11:53:41 +0900

    codeup

---
+++ codeup/6095.py
@@ -0,0 +1,8 @@
+pan = [[0] * 19 for _ in range(19)]
+i = int(input())
+for _ in range(i):
+    x, y = map(int, input().split())
+    pan[x-1][y-1] = 1
+
+for row in pan:
+    print(' '.join(map(str, row)))
