a2238137f2d6a6795fc2e9df84be51ecf0c0abf5
hyojun6 <yhyojun66@gmail.com>
2024-09-28 23:04:03 +0900

    codeup

--- codeup/6096.py
+++ codeup/6096.py
@@ -1,11 +1,20 @@
 pan = [[0] * 19 for _ in range(19)]
-pan = map(int, input().split())
-print(pan)
 
-# i = int(input())
-# for _ in range(i):
-#     x, y = map(int, input().split())
-#     pan[x-1][y-1] = 1
-# for row in pan:
-#     print(' '.join(map(str, row)))
+for i in range(19):
+    pan[i] = list(map(int, input().split()))
+
+n = int(input())
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    x -= 1
+    y -= 1
     
+    for j in range(19):
+        pan[x][j] = 1 - pan[x][j]
+    
+    for j in range(19):
+        pan[j][y] = 1 - pan[j][y]
+
+for row in pan:
+    print(*row)
