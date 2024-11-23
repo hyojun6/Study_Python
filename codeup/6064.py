f2bcf13525bc4ca8e66f48122f8720df802b801a
hyojun6 <yhyojun66@gmail.com>
2024-11-24 00:06:55 +0900

    codeup

--- codeup/6064.py
+++ codeup/6064.py
@@ -1,8 +1,4 @@
-a, b, c = input().split(' ')
-a = int(a)
-b = int(b)
-c = int(c)
-#a, b, c = map(int, input().split(' '))
+a, b, c = map(int, input().split(' '))
 if((a<b) & (a<c)):
     print(a)
 elif((b<a) & (b<c)):
